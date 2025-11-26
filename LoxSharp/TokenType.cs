f08673d24b6a83a152a023389bc14c64301bf015
Lars Thorben Dammann <larstd90@gmail.com>
2025-11-26 14:17:10 +0100

    Added TokenType

---
+++ LoxSharp/TokenType.cs
@@ -0,0 +1,52 @@
+﻿namespace LoxSharp;
+
+enum TokenType
+{
+    // single character tokens
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // one or two characters
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // literals
+    Identifier,
+    String,
+    Number,
+
+    // keywords
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    EOF,
+}
