eec93ebec397bc19b701bc6fa5f3d7c20600dec7
Lars Thorben Dammann <larstd90@gmail.com>
2025-11-26 13:23:51 +0100

    Can run a file or interactively

--- LoxSharp/Program.cs
+++ LoxSharp/Program.cs
@@ -1,9 +1,44 @@
-﻿namespace LoxSharp;
+﻿
+namespace LoxSharp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args.Length == 0)
+        {
+            RunInteractively();
+        }
+        else if (args.Length == 1)
+        {
+            RunFile(args[0]);
+        }
+        else
+        {
+            Console.WriteLine($"Usage: LoxSharp [file]");
+        }
+    }
+
+    private static void RunInteractively()
+    {
+        while (GetInput() is string line)
+            Run(line);
+    }
+
+    private static string? GetInput()
+    {
+        Console.Write("> ");
+        return Console.ReadLine();
+    }
+
+    private static void RunFile(string path)
+    {
+        string source = File.ReadAllText(path);
+        Run(source);
+    }
+
+    private static void Run(string source)
+    {
+        Console.WriteLine(source);
     }
 }
