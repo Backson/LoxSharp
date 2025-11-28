4017ee58f52e6bb432e8008a739b564cf7fd0185
Lars Thorben Dammann <larstd90@gmail.com>
2025-11-28 21:36:37 +0100

    Added expression AST

---
+++ LoxSharp/Ast/Ast.cs
@@ -0,0 +1,58 @@
+﻿namespace LoxSharp.Ast;
+
+interface IExpressionVisitor<T>
+{
+    T VisitBinaryExpression(BinaryExpression expr);
+    T VisitUnaryExpression(UnaryExpression expr);
+    T VisitGroupedExpression(GroupedExpression expr);
+    T VisitLiteralExpression(LiteralExpression expr);
+}
+
+abstract class Expression
+{
+    // Visitor pattern
+    public abstract T Accept<T>(IExpressionVisitor<T> visitor);
+}
+
+class BinaryExpression : Expression
+{
+    public required Expression Left { get; init; }
+    public required Token Operator { get; init; }
+    public required Expression Right { get; init; }
+
+    public override T Accept<T>(IExpressionVisitor<T> visitor)
+    {
+        return visitor.VisitBinaryExpression(this);
+    }
+}
+
+class UnaryExpression : Expression
+{
+    public required Token Operator { get; init; }
+    public required Expression Child { get; init; }
+
+    public override T Accept<T>(IExpressionVisitor<T> visitor)
+    {
+        return visitor.VisitUnaryExpression(this);
+    }
+}
+
+class GroupedExpression : Expression
+{
+    public required Expression Child { get; init; }
+
+    public override T Accept<T>(IExpressionVisitor<T> visitor)
+    {
+        return visitor.VisitGroupedExpression(this);
+    }
+}
+
+class LiteralExpression : Expression
+{
+    public required Token Value { get; init; }
+
+    public override T Accept<T>(IExpressionVisitor<T> visitor)
+    {
+        return visitor.VisitLiteralExpression(this);
+    }
+}
---
+++ LoxSharp/Ast/PrettyPrinter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoxSharp.Ast;
+
+class PrettyPrinter : IExpressionVisitor<string>
+{
+    public string VisitBinaryExpression(BinaryExpression expr)
+    {
+        string op = expr.Operator.Text;
+        string left = expr.Left.Accept(this);
+        string right = expr.Right.Accept(this);
+        return $"({op} {left} {right})";
+    }
+
+    public string VisitGroupedExpression(GroupedExpression expr)
+    {
+        string child = expr.Child.Accept(this);
+        return $"(group {child})";
+    }
+
+    public string VisitLiteralExpression(LiteralExpression expr)
+    {
+        return expr.Value.Text;
+    }
+
+    public string VisitUnaryExpression(UnaryExpression expr)
+    {
+        string op = expr.Operator.Text;
+        string child = expr.Child.Accept(this);
+        return $"({op} {child})";
+    }
+}
